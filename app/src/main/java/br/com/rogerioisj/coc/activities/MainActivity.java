0b1d90e3687f4ec0a3798f954e6c4c034da3ae02
Roger <rogerio.reaper@gmail.com>
2020-09-18 16:24:23 -0300

    Implementado segunda tela e  retorno

--- app/src/main/java/br/com/rogerioisj/coc/activities/MainActivity.java
+++ app/src/main/java/br/com/rogerioisj/coc/activities/MainActivity.java
@@ -2,15 +2,42 @@ package br.com.rogerioisj.coc.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 import br.com.rogerioisj.coc.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button botaoJogar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = findViewById(R.id.main_botao_jogar);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Resultado.class);
+
+                intent.putExtra("jogada", defineRodada());
+
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    int defineRodada(){
+        int jogada = new Random().nextInt(2);
+
+        return jogada;
     }
 }
--- app/src/main/java/br/com/rogerioisj/coc/activities/Resultado.java
+++ app/src/main/java/br/com/rogerioisj/coc/activities/Resultado.java
@@ -2,15 +2,48 @@ package br.com.rogerioisj.coc.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import br.com.rogerioisj.coc.R;
 
 public class Resultado extends AppCompatActivity {
 
+    ImageView imagemMoeda;
+    Button botaoVolta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        imagemMoeda = findViewById(R.id.resultado_imagem);
+
+        Bundle dados = getIntent().getExtras();
+
+        int jogada = dados.getInt("jogada");
+
+        defineLadoMoeda(jogada);
+
+        botaoVolta = findViewById(R.id.resultado_botao_voltar);
+        botaoVolta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void defineLadoMoeda(int jogada) {
+        if (jogada == 0) {
+            System.out.println("Jogada: Cara");
+            imagemMoeda.setImageResource(R.drawable.moeda_cara);
+        } else {
+            System.out.println("Jogada: Coroa");
+            imagemMoeda.setImageResource(R.drawable.moeda_coroa);
+        }
     }
 }
